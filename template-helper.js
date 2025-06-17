39a6de54a6b0c481185025178e55e6951026d695
Abid R. <Abydars@users.noreply.github.com>
2025-06-17 06:51:09 +0500

    Update template-helper.js

--- template-helper.js
+++ template-helper.js
@@ -8,7 +8,7 @@ const LesscodeTemplateHelper = {
               ahref.href=`https://bubble.io/template/${marketplaceUrl}`;
               ahref.innerText = "Buy this template";
               ahref.style="padding: 10px;background: #0a7cac;border-radius: 5px;color: white;display: inline-block;";
-              acon.style="padding: 15px;text-align: right;display: block;background: #ebfdff;border: dotted 2px #DDD;margin: 15px;border-radius: 10px;";
+              acon.style="padding: 15px;text-align: right;display: flex;background: rgb(33 185 202 / 12%);border: 2px dotted rgb(0 0 0 / 12%);margin: 15px;border-radius: 10px;position: fixed;top: 0;right: 0;left: 0;z-index: 2002;";
               acon.appendChild(ahref);
               document.body.appendChild(acon);
           }
