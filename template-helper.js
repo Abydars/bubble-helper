cc82eeee0feed1e9dd1cdcc84bbdb98f1640d5b2
Abid R. <Abydars@users.noreply.github.com>
2025-06-17 05:47:47 +0500

    Update template-helper.js

--- template-helper.js
+++ template-helper.js
@@ -1,11 +1,11 @@
 const LesscodeTemplateHelper = {
-  topBar: function(domain) {
+  topBar: function(domain, marketplaceUrl) {
     document.addEventListener('DOMContentLoaded', function() {
           if(window.location.href.indexOf(domain) > 0) {
               var acon = document.createElement('div');
               var ahref = document.createElement('a');
               ahref.id="buytemplate";
-              ahref.href="";
+              ahref.href=`https://bubble.io/template/${marketplaceUrl}`;
               ahref.innerText = "Buy this template";
               ahref.style="padding: 10px;background: #0a7cac;border-radius: 5px;color: white;display: inline-block;";
               acon.style="padding: 15px;text-align: right;display: block;background: #ebfdff;border: dotted 2px #DDD;margin: 15px;border-radius: 10px;";
