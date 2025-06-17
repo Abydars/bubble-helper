5f81d6544e8c673bd2a946e13e81cac3189446ce
Abid R. <Abydars@users.noreply.github.com>
2025-06-17 07:14:16 +0500

    Update template-helper.js

--- template-helper.js
+++ template-helper.js
@@ -14,10 +14,10 @@ const LesscodeTemplateHelper = {
           topBar.fadeOut();
         });
         
-        var buyHref = $(`<a href="https://bubble.io/template/${marketplaceUrl}">Buy this template</a>`);
+        var buyHref = $(`<a target="_blank" href="https://bubble.io/template/${marketplaceUrl}">Buy this template</a>`);
         buyHref.attr('style', btnStyles);
 
-        var profileHref = $(`<a href="https://bubble.io/agency/lesscodeio">About Lesscode.io</a>`);
+        var profileHref = $(`<a target="_blank" href="https://bubble.io/agency/lesscodeio">About Lesscode.io</a>`);
         profileHref.attr('style', btnStyles);
         
         topBar.attr('style', 'gap: 10px; padding: 15px;text-align: right;display: flex;background: rgb(33 185 202 / 12%);border: 2px dotted rgb(0 0 0 / 12%);margin: 15px;border-radius: 10px;position: fixed;top: 0;right: 0;left: 0;z-index: 2002;');
