39393875b8459f963112753491936386d2913752
Abid R. <Abydars@users.noreply.github.com>
2025-06-17 07:08:59 +0500

    Update template-helper.js

--- template-helper.js
+++ template-helper.js
@@ -11,7 +11,7 @@ const LesscodeTemplateHelper = {
         var profileHref = $(`<a href="https://bubble.io/contributor/1685979404807x551826349360229570">About Lesscode.io</a>`);
         profileHref.attr('style', 'padding: 10px;background: #0a7cac;border-radius: 5px;color: white;display: inline-block;');
         
-        topBar.attr('style', 'padding: 15px;text-align: right;display: flex;background: rgb(33 185 202 / 12%);border: 2px dotted rgb(0 0 0 / 12%);margin: 15px;border-radius: 10px;position: fixed;top: 0;right: 0;left: 0;z-index: 2002;');
+        topBar.attr('style', 'gap: 10px; padding: 15px;text-align: right;display: flex;background: rgb(33 185 202 / 12%);border: 2px dotted rgb(0 0 0 / 12%);margin: 15px;border-radius: 10px;position: fixed;top: 0;right: 0;left: 0;z-index: 2002;');
         topBar.append(buyHref);
         topBar.append(profileHref);
         
