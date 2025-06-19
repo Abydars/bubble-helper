11093ea18f4d5c092dffe65f42242bfdfa19d235
Abid R. <Abydars@users.noreply.github.com>
2025-06-19 05:46:45 +0500

    Update template-helper.js

--- template-helper.js
+++ template-helper.js
@@ -15,43 +15,44 @@ const LesscodeTemplateHelper = {
         this.currentDemo = v;
       }
     });
-    this.topBar();
+
+    if(this.currentDemo) {
+      this.topBar();
+    }
   },
   topBar: function() {
     var $ = jQuery;
     
     document.addEventListener('DOMContentLoaded', () => {
-      if(this.currentDemo) {
-        const marketplaceUrl = this.currentDemo.marketplace;
-        var topBar = $('<div/>');
-        var btnStyles = 'padding: 10px 20px;background: none;border-radius: 30px;color: #0a7cac;font-size: 14px;display: inline-block;font-weight: 600;border: 1px solid #0a7cac;text-decoration: none !important;';
+      const marketplaceUrl = this.currentDemo.marketplace;
+      var topBar = $('<div/>');
+      var btnStyles = 'padding: 10px 20px;background: none;border-radius: 30px;color: #0a7cac;font-size: 14px;display: inline-block;font-weight: 600;border: 1px solid #0a7cac;text-decoration: none !important;';
 
-        var closeBtn = $(`<a href="#">Close</a>`);
-        closeBtn.attr('style', btnStyles);
-        closeBtn.on('click', function(e) {
-          e.preventDefault();
-          topBar.slideUp();
-        });
-        
-        var buyHref = $(`<a target="_blank" href="https://bubble.io/template/${marketplaceUrl}">Buy this template</a>`);
-        buyHref.attr('style', btnStyles);
+      var closeBtn = $(`<a href="#">Close</a>`);
+      closeBtn.attr('style', btnStyles);
+      closeBtn.on('click', function(e) {
+        e.preventDefault();
+        topBar.slideUp();
+      });
+      
+      var buyHref = $(`<a target="_blank" href="https://bubble.io/template/${marketplaceUrl}">Buy this template</a>`);
+      buyHref.attr('style', btnStyles);
 
-        var profileHref = $(`<a target="_blank" href="https://bubble.io/agency/lesscodeio">About Lesscode.io</a>`);
-        profileHref.attr('style', btnStyles);
-        
-        topBar.attr('style', 'gap: 10px;padding: 15px;justify-content: end;display: flex;background: rgba(33, 185, 202, 0.12);border: 2px dotted rgba(0, 0, 0, 0.12);margin: 15px;border-radius: 10px;position: static;top: 0px;right: 0px;left: 0px;z-index: 2002;');
-        
-        topBar.append(buyHref);
-        topBar.append(profileHref);
-        topBar.append(closeBtn);
-        topBar.hide();
-        
-        $('body').append(topBar);
+      var profileHref = $(`<a target="_blank" href="https://bubble.io/agency/lesscodeio">About Lesscode.io</a>`);
+      profileHref.attr('style', btnStyles);
+      
+      topBar.attr('style', 'gap: 10px;padding: 15px;justify-content: end;display: flex;background: rgba(33, 185, 202, 0.12);border: 2px dotted rgba(0, 0, 0, 0.12);margin: 15px;border-radius: 10px;position: static;top: 0px;right: 0px;left: 0px;z-index: 2002;');
+      
+      topBar.append(buyHref);
+      topBar.append(profileHref);
+      topBar.append(closeBtn);
+      topBar.hide();
+      
+      $('body').append(topBar);
 
-        setTimeout(function() {
-          topBar.slideDown();
-        }, 3000);
-      }
+      setTimeout(function() {
+        topBar.slideDown();
+      }, 3000);
     });
   }
 };
